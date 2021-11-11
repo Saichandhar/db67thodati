ea38805525b8aa8d939acd505f661f412ffaa219
Thodati, Sai Chandhar Reddy <57518509+Saichandhar@users.noreply.github.com>
2021-11-10 21:09:07 -0600

    Update app.js

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ app.use('/users', usersRouter);
 app.use('/earphones', earphonesRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
+app.use('/resource', resourceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
