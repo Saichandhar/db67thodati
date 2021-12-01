db9a2fd18a49dccb3f15da7f377e9347a8370d37
Thodati, Sai Chandhar Reddy <57518509+Saichandhar@users.noreply.github.com>
2021-11-30 20:15:06 -0600

    added authentication

--- routes/earphone.js
+++ routes/earphone.js
@@ -1,6 +1,13 @@
 var express = require('express');
 const earphone_controlers= require('../controllers/earphone');
 var router = express.Router();
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 
 /* GET home page. */
 router.get('/', earphone_controlers.earphone_view_all_Page);
@@ -12,7 +19,7 @@ router.get('/detail', earphone_controlers.earphone_view_one_Page);
 router.get('/create', earphone_controlers.earphone_create_Page);
 
 /* GET create update page */ 
-router.get('/update', earphone_controlers.earphone_update_Page);  
+router.get('/update',secured, earphone_controlers.earphone_update_Page);  
  
 /* GET create earphone page */ 
 router.get('/delete', earphone_controlers.earphone_delete_Page); 
